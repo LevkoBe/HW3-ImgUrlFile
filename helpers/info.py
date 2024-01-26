a0820f11e713cc2987f2b7999d977ba910897b71
Levko Beniakh <lbeniakh@kse.org.ua>
2024-01-26 13:58:13 +0200

    HTML file update

--- helpers/info.py
+++ helpers/info.py
@@ -1,10 +1,31 @@
+# helpers/info.py
+
 info = {
     "endpoints": [
         {
             "path": "/",
             "method": "GET",
             "description": "Get info about the endpoints, as well as about the website."
+        },
+        {
+            "path": "/info",
+            "method": "GET",
+            "description": "Get information provided by the server."
+        },
+        {
+            "path": "/image/{image_name}",
+            "method": "GET",
+            "description": "Get a specific image by providing the image name in the path."
+        },
+        {
+            "path": "/urlParse?url={url_string}",
+            "method": "GET",
+            "description": "Parse and get information about the provided URL."
+        },
+        {
+            "path": "/fileParse",
+            "method": "POST",
+            "description": "Parse the content of a file and provide metadata based on user input."
         }
     ]
 }
-
